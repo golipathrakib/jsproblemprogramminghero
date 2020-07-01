ab59c5c2f9931d1cffa2c7a43996efec8d5b0692
golipathrakib <rakib.488125@gmail.com>
2020-07-02 00:13:26 +0600

    random js

---
+++ random.js
@@ -0,0 +1,17 @@
+var num = 2.5999999;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+var randomNum = Math.random() * 10;
+var output = Math.round(randomNum);
+//console.log(result);
+//console.log(result2);
+//console.log(result3);
+//console.log(randomNum);
+//console.log(output);
+
+for(var i = 0; i < 5; i++){
+    var randomNum = Math.random() * 6;
+    var output = Math.round(randomNum);
+    console.log(output);
+}
